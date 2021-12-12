dfb748205b4092e2bc9f438ba26d144be3f3c35e
Shivam Sharma <ShivamSharmaSVS@gmail.com>
2021-12-12 13:30:22 +0530

    Initial commit

---
+++ src/main/java/com/wergroot/kamartaj/KamartajApplication.java
@@ -0,0 +1,13 @@
+package com.wergroot.kamartaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamartajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamartajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wergroot/kamartaj/KamartajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wergroot.kamartaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamartajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
